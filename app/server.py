5656d23219d79f2a494c46b7d54b81182c4e5bbb
Josh <35052907+yeungjosh@users.noreply.github.com>
2020-07-05 20:59:21 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,9 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=10YSp0BRe4VT1V271B-Jg3h8V1htYykDM'
+
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
